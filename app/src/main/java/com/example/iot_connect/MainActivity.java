e33582e037710ae2cb401b01bb7ed614ec6a8b22
Thiago Bastos <126490535+Thiago-bsts@users.noreply.github.com>
2024-06-11 16:37:26 -0300

    front-incompleto

--- app/src/main/java/com/example/iot_connect/MainActivity.java
+++ app/src/main/java/com/example/iot_connect/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.iot_connect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Switch botaoSwitch;
+    private ToggleButton botaoToogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoSwitch = findViewById(R.id.btnswitch);
+    //    botaoToogle = findViewById(R.id.btnToogle);
+    }
+
+    public void btnListner(){
+        botaoSwitch.setOnCheckedChangeListener((buttonView, isChecked));
     }
 }
